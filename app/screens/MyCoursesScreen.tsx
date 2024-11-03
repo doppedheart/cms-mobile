1410a4b2d95b088953f27ee68071ac57e59a72a3
doppedheart <anuragagarwal530@gmail>
2024-11-04 01:18:36 +0530

    Removed scrollbar and added compatiblity with listView

--- app/screens/MyCoursesScreen.tsx
+++ app/screens/MyCoursesScreen.tsx
@@ -1,15 +1,22 @@
 import React, { FC, useRef } from "react"
 import { observer } from "mobx-react-lite"
-import { Animated, ScrollView, TextStyle, View, ViewStyle } from "react-native"
-
-import { CourseCard, ListView, Screen, Text, TextField } from "app/components"
+import { Animated, TextStyle, View, ViewStyle } from "react-native"
+import {
+  CourseCard,
+  ListView,
+  ListViewProps,
+  ListViewRef,
+  Screen,
+  Text,
+  TextField,
+} from "app/components"
 import { colors, spacing, typography } from "app/theme"
 import { TabScreenProps } from "app/navigators"
 
 interface MyCoursesScreenProps extends TabScreenProps<"MyCourses"> {}
 
-const renderCourses = () => {
-  return <CourseCard />
+interface ListItem {
+  type: "header" | "course" | "searchBar"
 }
 
 const renderSeparator = () => {
@@ -19,37 +26,56 @@ const renderSeparator = () => {
 export const MyCoursesScreen: FC<MyCoursesScreenProps> = observer(function MyCoursesScreen() {
   // const { userStore } = useStores()
   const scrollY = useRef(new Animated.Value(0)).current
-  const username = undefined
+  const listRef = useRef<ListViewRef<ListItem>>(null)
+  const username: string | undefined = undefined
 
-  return (
-    <Screen contentContainerStyle={$root} safeAreaEdges={["top"]}>
-      <ScrollView
-        stickyHeaderIndices={[2]}
-        scrollEventThrottle={14}
-        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
-          useNativeDriver: false,
-        })}
-      >
+  const renderItem: ListViewProps<ListItem>["renderItem"] = ({ item, index }) => {
+    if (index === 0) {
+      return (
         <View>
           {/* Add user avatar here */}
           <Text preset="subheading" style={$greeting} text={`Hi, ${username ?? "Hustler"}`} />
+          <Text preset="heading" style={$heading} text="My Courses" />
         </View>
-        <Text preset="heading" style={$heading} text="My Courses" />
-        <TextField
-          inputWrapperStyle={$searchBox}
-          style={$searchBarWrapper}
-          placeholder="Search videos..."
-        />
-        <ListView
-          // keyExtractor={(item) => item.id}
-          contentContainerStyle={$listContainer}
-          data={[{}, {}, {}, {}, {}]}
-          estimatedItemSize={10}
-          ItemSeparatorComponent={renderSeparator}
-          renderItem={renderCourses}
-          showsVerticalScrollIndicator={false}
-        />
-      </ScrollView>
+      )
+    } else if (index === 1) {
+      return (
+        <View style={$searchBoxContainer}>
+          <TextField
+            inputWrapperStyle={$searchBox}
+            style={$searchBarWrapper}
+            placeholder="Search videos..."
+          />
+        </View>
+      )
+    } else {
+      return <CourseCard />
+    }
+  }
+
+  const data: ListItem[] = [
+    { type: "header" },
+    { type: "searchBar" },
+    ...Array(5).fill({ type: "course" }),
+  ]
+
+  return (
+    <Screen contentContainerStyle={$root} safeAreaEdges={["top"]}>
+      <ListView
+        ref={listRef}
+        contentContainerStyle={$listContainer}
+        data={data}
+        estimatedItemSize={100}
+        ItemSeparatorComponent={renderSeparator}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        stickyHeaderIndices={[1]}
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
+          useNativeDriver: false,
+        })}
+        scrollEventThrottle={16}
+        keyExtractor={(item, index) => item.type + index.toString()}
+      />
     </Screen>
   )
 })
@@ -63,7 +89,7 @@ const $greeting: TextStyle = {
 }
 
 const $heading: TextStyle = {
-  marginVertical: spacing.md,
+  marginTop: spacing.md,
 }
 
 const $listContainer: ViewStyle = {
@@ -80,5 +106,9 @@ const $searchBarWrapper: ViewStyle = {
 
 const $searchBox: ViewStyle = {
   backgroundColor: colors.background.primary,
-  marginVertical: 10,
+  marginVertical: spacing.xs,
+}
+
+const $searchBoxContainer: ViewStyle = {
+  backgroundColor: colors.background.primary,
 }
